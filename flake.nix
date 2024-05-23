5a35796003198955092c64390c59b2bb1bb52daa
geraldwuhoo <gerald@geraldwu.com>
2024-05-23 00:41:52 -0700

    migrate to rust-overlay flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        packages = with pkgs; [
+          (rust-bin.stable.latest.default.override {
+            extensions = [ "rust-src" "rust-analyzer" ];
+          })
+          pkg-config
+          cargo-audit
+          pre-commit
+        ];
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = packages;
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath packages}";
+        };
+      });
+}
--- shell.nix
+++
@@ -1,7 +0,0 @@
-let
-  pkgs = import <nixpkgs> { };
-  packages = with pkgs; [ pkg-config cargo-audit ];
-in pkgs.mkShell {
-  buildInputs = packages;
-  LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath packages}";
-}
